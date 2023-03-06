79343b0121571f555e5e40102284dfd04269bb02
victordanielmun <victordanielmun@gmail.com>
2023-03-05 22:43:35 -0500

    usuarios falsos usando mini faker se agrego  feed storys y story componentes

---
+++ components/Feed.js
@@ -0,0 +1,21 @@
+import Stories from "./Stories";
+
+
+export default function Feed() {
+  return (
+    <main>
+        <section>
+            {/* stories */}
+            <Stories />
+            {/* posts */}
+        </section>
+
+        <section>
+            {/* mini profile */}
+
+            {/* sugestions */}
+        </section>
+
+    </main>
+  )
+}
