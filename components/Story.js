90538619d7520ff5f174021f2c3d3dda8aea700d
victordanielmun <victordanielmun@gmail.com>
2023-03-06 20:29:45 -0500

    estilizado barra de historias con tailwind scrollbar

--- components/Story.js
+++ components/Story.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function Story({img, username}) {
+export default function Story({ img, username }) {
   return (
     <div>
-        <img src={img} alt={username} />
-        <p>{username}</p>
+      <img className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transform duration-200 ease-out" src={img} alt={username} />
+      <p className="text-xs w-14 truncate" >{username}</p>
     </div>
-  )
+  );
 }
