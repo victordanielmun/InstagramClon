0fdd8d29269dfe1c4c05aae0a2e5543d28de4f6d
victordanielmun <victordanielmun@gmail.com>
2023-03-06 21:16:10 -0500

    se agrega header e imagen al componente post

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,17 @@
-
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline"
 
 function Post({id, username, img, userImg, caption}) {
   return (
-    <div>
-    <h1>{username}</h1>
+    <div className="bg-white my-7 border rounded-md">
+    {/* post header */}
+    <div className="flex items-center p-5">
+        <img className="h-12 rounded-full object-cover border p-1 mr-3" src={userImg} alt={username}  />
+        <p className="font-bold flex-1 "> {username}</p>
+        <EllipsisHorizontalIcon className="h-5" />
+    </div>
+    <img className="object-cover w-full" src={img} alt="" />
+
+
     </div>
   )
 }
--- components/Posts.js
+++ components/Posts.js
@@ -5,14 +5,14 @@ function Posts() {
         {
             id: "1",
             username: "victor",
-            userImage: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+            userImg: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
             img: "https://images.unsplash.com/photo-1678107658617-d20a773469e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60",
             caption: "Nice picture",
         },
         {
             id: "1",
             username: "other",
-            userImage: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+            userImg: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
             img: "https://images.unsplash.com/photo-1678031410792-6b68d9e0bbd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
             caption: "picture x2",
         },
