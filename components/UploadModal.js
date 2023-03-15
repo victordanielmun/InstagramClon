46ba07b1544817b9c22e5b43c758405acc9c4d73
victordanielmun <victordanielmun@gmail.com>
2023-03-14 22:54:34 -0500

    Se completo diseño de modal para subir posts

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -1,16 +1,51 @@
 import { useRecoilState } from "recoil"
 import {modalState} from "../atom/modalAtom"
 import Modal from "react-modal"
+import{CameraIcon} from "@heroicons/react/24/outline";
+import { useRef, useState } from "react";
 
 export default function UploadModal() {
-    const [open, setOpen] = useRecoilState(modalState)
+    const [open, setOpen] = useRecoilState(modalState);
+    const filePickerRef = useRef(null);
+    const [selectedFile, setSelectedFile] = useState(null);
+    function addImageToPost(event){
+        const reader = new FileReader()
+        if (event.target.files[0]){
+          reader.readAsDataURL(event.target.files[0])
+        }
+        reader.onload = (readerEvent) => {
+          setSelectedFile(readerEvent.target.result)
+        }
+        
+    }
+
   return (
     <div><h1>Modal</h1>
     {open && (
       <Modal 
+      className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
       isOpen={open} 
-      onRequestClose={()=>setOpen(false)} >
+      onRequestClose={()=>{
+        setOpen(false);
+        setSelectedFile(null);
+        }} >
+        <div className="flex flex-col justify-center items-center h-[100%]">
+          {selectedFile ? (<img 
+          src={selectedFile} 
+          alt="imagen" 
+          onClick={()=>setSelectedFile(null)}
+          className="w-full max-h-[250px] object-cover cursor-pointer"
+          />):(<CameraIcon 
+        onClick={()=>filePickerRef.current.click()}
+        className="cursor-pointer h-14 bg-red-200  p-2 rounded-full border-2 text-red-500 "/>)}
         <h1>Modal</h1>
+        
+        <input type="file" hidden  ref={filePickerRef} onChange={addImageToPost} />
+        <input type="text"  maxLength="150" placeholder="Porfavor Ingresa tu comentario" className="m-4 border-none text-center w-full focus:rin-0 " /> 
+        <button disabled 
+        className=" w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disable:cursor-not-allowed disabled:hover:brightness-100" 
+        >Subir Post</button>
+        </div>
       </Modal>
     )}
     </div>
