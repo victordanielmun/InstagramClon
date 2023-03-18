59ce8cf0b8a53409ee8246bfaaf04ef55de74dbb
victordanielmun <victordanielmun@gmail.com>
2023-03-17 20:55:19 -0500

    se agrega funcionalidad comentarios desde firebase

--- components/Post.js
+++ components/Post.js
@@ -1,79 +1,130 @@
-import { EllipsisHorizontalIcon, HeartIcon, ChatBubbleOvalLeftEllipsisIcon ,BookmarkIcon, FaceSmileIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import {addDoc, collection, serverTimestamp} from "firebase/firestore";
+import {
+  EllipsisHorizontalIcon,
+  HeartIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  BookmarkIcon,
+  FaceSmileIcon,
+} from "@heroicons/react/24/outline";
+import { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { useRecoilState } from "recoil";
 import { userState } from "../atom/userAtom";
-import { useSession } from "next-auth/react"
+import { useSession } from "next-auth/react";
+import Moment, { moment } from "react-moment";
 
-function Post({id, username, img, userImg, caption}) {
+function Post({ id, username, img, userImg, caption }) {
   const [comment, setComment] = useState("");
-  const [currentUser] = useRecoilState(userState)
-  const {data: session} = useSession();
+  const [currentUser] = useRecoilState(userState);
+  const [comments, setComments] = useState([]);
+  const { data: session } = useSession();
 
-  async function sendComment(event){
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const unsubscribe = await onSnapshot(
+          query(
+            collection(db, "posts", id, "comments"),
+            orderBy("timestamp", "desc")
+          ),
+          (snapshot) => {
+            setComments(snapshot.docs);
+          }
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [db, id]);
+
+  async function sendComment(event) {
     event.preventDefault();
     const commentToSend = comment;
-    setComment("")
+    setComment("");
     await addDoc(collection(db, "posts", id, "comments"), {
       comment: commentToSend,
       username: session.user.username,
       userImage: session.user.image,
       timestamp: serverTimestamp(),
-    })
+    });
   }
 
-    return (
+  return (
     <div className="bg-white my-7 border rounded-md">
-    {/* post header */}
-    <div className="flex items-center p-5">
-        <img 
-        className="h-12 rounded-full object-cover border p-1 mr-3" 
-        src={userImg} 
-        alt={username}  
+      {/* post header */}
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
         />
         <p className="font-bold flex-1 "> {username}</p>
         <EllipsisHorizontalIcon className="h-5" />
-    </div>
-        {/* post imagen */}
-    <img className="object-cover w-full" src={img} alt="" />
-        {/* post buttons */}
+      </div>
+      {/* post imagen */}
+      <img className="object-cover w-full" src={img} alt="" />
+      {/* post buttons */}
 
-        {session && (
+      {session && (
         <div className="flex justify-between px-4 pt-4">
-        <div className="flex space-x-4">
+          <div className="flex space-x-4">
             <HeartIcon className="btn" />
             <ChatBubbleOvalLeftEllipsisIcon className="btn" />
-            
+          </div>
+          <BookmarkIcon className="btn" />
         </div>
-        <BookmarkIcon className="btn" />
+      )}
+
+      {/* Post comments */}
+      <p className="p-5 truncate">
+        <span className=" font-bold mr-2 ">{username}</span>
+        {caption}
+      </p>
+      {comments.length > 0 && (
+        <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+          {comments.map((comment) => (
+            <div 
+            className="flex items-center space-x-2 mb-2" 
+            key={comment.data().id}
+            >
+              <img className="h-7 rounded-full object-cover" src={comment.data().userImage} alt="user-image" />
+              <p className="font-semibold ">{comment.data().comment}</p>
+              <p className="flex-1 truncate ">{comment.data().username}</p>
+              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
         </div>
-        )}
-    
-     {/* Post comments */}
-     <p className="p-5 truncate" ><span className=" font-bold mr-2 " >{username}</span>{caption}</p>
-     {session && (
-     <form className="flex items-center p-4 " >
-        <FaceSmileIcon className="h-7" />
-        <input 
-        value={comment} 
-        onChange={(event)=>setComment(event.target.value)}
-        className="border-none flex-1 focus:ring-0" 
-        type="text" 
-        placeholder="Enter your comment..." 
-        />
-        <button 
-        type="submit"
-        onClick={sendComment}
-        disabled={!comment.trim()} 
-        className="text-blue-400 font-bold disabled:text-blue-200" >Post</button>
-     </form>
-     )}
-     
-    
+      )}
+      {session && (
+        <form className="flex items-center p-4 ">
+          <FaceSmileIcon className="h-7" />
+          <input
+            value={comment}
+            onChange={(event) => setComment(event.target.value)}
+            className="border-none flex-1 focus:ring-0"
+            type="text"
+            placeholder="Enter your comment..."
+          />
+          <button
+            type="submit"
+            onClick={sendComment} 
+            disabled={!comment.trim()} 
+            className="text-blue-400 font-bold disabled:text-blue-200"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
-  )
+  );
 }
 
-export default Post
-
+export default Post;
